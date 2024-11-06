c75514a57b01f35a526684e076025f3749807878
BHTY <phasershow@gmail.com>
2024-11-05 23:00:57 -0500

    slab -> slab_cache

--- kernel/include/mm/slab.h
+++ kernel/include/mm/slab.h
@@ -3,14 +3,14 @@
 #include <stdint.h>
 #include <stddef.h>
 
-typedef struct slab {
+typedef struct slab_cache {
     uint32_t elements;
     size_t element_size;
     void* data;
     uint32_t bitmap[0];
-} slab_t;
+} slab_cache_t;
 
-slab_t* slab_create(uint32_t elements, size_t element_size, void* data);
-void* slab_alloc(slab_t* slab);
-void slab_release(slab_t* slab, void* data);
-void slab_destroy(slab_t* slab);
+slab_cache_t* slab_create(uint32_t elements, size_t element_size, void* data);
+void* slab_alloc(slab_cache_t* slab);
+void slab_release(slab_cache_t* slab, void* data);
+void slab_destroy(slab_cache_t* slab);
--- kernel/src/mm/slab.c
+++ kernel/src/mm/slab.c
@@ -5,5 +5,3 @@
 */
 
 #include <mm/slab.h>
-
-
